99b15999786c09bf19741a53c62d8cf45e689b88
OdedMous <68702877+OdedMous@users.noreply.github.com>
2021-07-05 12:36:22 +0300

    Update Insurance_prediction.R

--- Insurance_prediction.R
+++ Insurance_prediction.R
@@ -14,7 +14,17 @@ libary(ggplot2)
 
 
 
-
+pie_chart <- function(column, title) {
+  temp = count(column)
+  ggplot(temp, aes(x="", y=freq, fill=x)) +
+  geom_bar(stat="identity", width=1, color="white") +
+  coord_polar("y", start=0) +
+  theme_void()+ # remove background, grid, numeric labels
+  geom_text(aes(y = freq/length(freq) + c(0, cumsum(freq)[-length(freq)]),
+                label = paste0(round(freq*100/sum(freq)), "%")), size=5)+
+  guides(fill=guide_legend(title=title))
+  
+}
 
 
 
